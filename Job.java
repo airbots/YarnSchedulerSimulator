89b4dcc0483a9d5fcb90512ae9f0513d69cebe9f
Chen He <chenhe@yahoo-inc.com>
2014-09-14 17:50:26 -0500

    Job is added

---
+++ Job.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Chen He
+ * Date: 9/14/14
+ * Time: 5:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Job {
+
+}
