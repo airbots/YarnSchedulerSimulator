f608735b7f519e7d0b021b7b10c92ff252a74631
Chen He <chenhe@yahoo-inc.com>
2014-09-24 10:58:46 -0500

    scheduler class is added

---
+++ Scheduler.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenhe
+ * Date: 9/24/14
+ * Time: 10:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Scheduler {
+  Collections<Job> jobQueue = new Collections<Job>();
+}
