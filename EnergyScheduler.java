58c3dc180f7a8374ffefcbe45ae85c35fd0b4cb7
Chen He <chenhe@yahoo-inc.com>
2014-09-25 22:57:04 -0500

    add 2 schedulers: fifo and energy

---
+++ EnergyScheduler.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenhe
+ * Date: 9/25/14
+ * Time: 10:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EnergyScheduler extends Scheduler {
+}
---
+++ FifoScheduler.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenhe
+ * Date: 9/25/14
+ * Time: 10:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FifoScheduler extends Scheduler {
+
+}
